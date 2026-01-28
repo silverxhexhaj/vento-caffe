d672276ac5a77b56b9e85fc25cc251bb2781fc18
silverxhexhaj <xhexhajsilver@gmail.com>
2026-01-29 00:38:42 +0100

    Remove FreeMachineOffer component from Home page for a cleaner layout

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,12 +1,10 @@
 import Hero from "@/components/home/Hero";
-import FreeMachineOffer from "@/components/home/FreeMachineOffer";
 import ProductPreview from "@/components/home/ProductPreview";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <FreeMachineOffer />
       <ProductPreview />
     </>
   );
